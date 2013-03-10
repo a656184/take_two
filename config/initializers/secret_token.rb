3673f30bb1f0fb138bd9418314d43795fd2011b2
Eric Kristensen <eric.l.kristensen@gmail.com>
2013-03-10 13:04:58 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TakeTwo::Application.config.secret_token = '524db63096407382e3ea9f0dafb4eecca88709c4b8b0f74e7507060df53d855ae4da67ed5677e8a45fce9db40df72ae43f3c6b9f1fd26262db39000585ed4bef'
